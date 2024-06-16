62252800f6d98fa59c26243549ea26af86858921
Santi Tabbach <tabbach97@gmail.com>
2024-06-15 21:41:06 -0300

    Add server.js file

---
+++ server.js
@@ -0,0 +1,57 @@
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const path = require("path");
+const { logger, logEvents } = require("./middleware/logger");
+const errorHandler = require("./middleware/errorHandler");
+const corsOptions = require("./config/corsOptions");
+const connectDB = require("./config/dbConn");
+
+const PORT = process.env.PORT || 3500;
+
+const app = express();
+
+connectDB();
+
+//Order matters -> first loggers, then middlewares, last error handlers
+app.use(logger);
+
+app.use(cors(corsOptions));
+
+app.use(express.json());
+
+app.use(cookieParser());
+
+app.use("/", express.static(path.join(__dirname, "public")));
+
+app.use("/", require("./routes/root"));
+app.use("/auth", require("./routes/authRoutes"));
+app.use("/users", require("./routes/userRoutes"));
+app.use("/tasks", require("./routes/taskRoutes"));
+
+app.all("*", (req, res) => {
+  res.status(404);
+  if (req.accepts("html")) {
+    res.sendFile(path.join(__dirname, "views", "404.html"));
+  } else if (req.accepts("json")) {
+    res.json({ message: "404 Not Found" });
+  } else {
+    res.type("txt").send("404 Not Found");
+  }
+});
+
+app.use(errorHandler);
+
+mongoose.connection.once("open", () => {
+  console.log("Connected to Mongo DB");
+  app.listen(PORT, () => console.log(`server running on port ${PORT}`));
+});
+
+mongoose.connection.on("error", (error) => {
+  logEvents(
+    `${error.no}: ${error.code}\t${error.syscall}\t${error.hostname}`,
+    "mongoErrLog.log"
+  );
+});
